29b75a87726e059b40b2949e371091119b6fd690
Piotr Lasota <lasota.piotr1@gmail.com>
2017-03-17 16:51:27 +0100

    finished clock project

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -11,9 +11,34 @@
   function setDate(hands) {
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = seconds*6 + 90;
-    hands.secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const mins = now.getMinutes();
+    const hours = now.getHours();
 
+
+    if (seconds === 0) {
+      hands.secondHand.style.transitionDuration = '0s';
+    } else {
+      hands.secondHand.style.transitionDuration = '0.3s';
+    }
+    if (mins === 0) {
+      hands.minHand.style.transitionDuration = '0s';
+    } else {
+      hands.minHand.style.transitionDuration = '0.3s';
+    }
+    if (hours === 0) {
+      hands.hourHand.style.transitionDuration = '0s';
+    } else {
+      hands.hourHand.style.transitionDuration = '0.3s';
+    }
+
+
+    const secondsDegrees = seconds * 6 + 90;
+    const minsDegrees = mins * 6 + 90;
+    const hoursDegrees = hours * 30 + 90;
+
+    hands.secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    hands.minHand.style.transform = `rotate(${minsDegrees}deg)`;
+    hands.hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
   }
 
 
